cdd1d0e31f6842a6d5f76e4f5f9067235a09d2e4
devRSV <devRSV@d.c>
2023-10-04 01:34:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ DATABASE_URL = 'sqlite+aiosqlite:///bot.sqlite3'
 TIME_ZONE = 'Europe/Berlin'
 REPORT_REQUEST_TIME = 18
 DAILY_REPORT_TIME = 20
-SERVER_ID = 1158451748806074439
+SERVER_ID = 0
 CATEGORY_NAME = 'Daily Reports'
 CHANNELS_ROLES_NAMES_DICT = {
     'back-end': 'Back End',
